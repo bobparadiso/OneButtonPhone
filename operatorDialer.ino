780b3e9259345d6dc50b4d9cdbaaa26524ed9395
bobparadiso <bobparadiso@gmail.com>
2017-05-28 11:51:36 -0400

    -cleanup and switch from mechanical to solid state relays

--- operatorDialer.ino
+++ operatorDialer.ino
@@ -1,20 +1,20 @@
 //-code to answer/hang up phone and optionally dial operator via a single switch
-//-running on Arduino Nano (w/ Optiboot) wired to Comdial 2500 phone via relays
+//-running on Arduino Nano (w/ Optiboot) wired to Comdial 2500 phone via SSRs
 //-any model phone could be used in concept, as long as relay(s) are in place for hook and a relay for '0' key, then SetLineOpen function is modified appropriately
 
 //for Comdial 2500 hook wiring:
-//for hook down: yellow->gray, black->red
-//for hook up: yellow->brown, green->white
+//for hook down: yellow(L2)->gray(L2), black(GN)->red(R)
+//for hook up: yellow(L2)->brown(J2), green(L1)->white(J1)
 
 #include <avr/wdt.h>
 #include <TimerOne.h>
 
-#define HOOK0_PIN 2
-#define HOOK1_PIN 3
-#define HOOK2_PIN 4
-#define DIAL_0_PIN 5
-#define SWITCH_PIN 6 //PD6 PCINT22
-#define LED_PIN 12 //off-hook indicator LED
+#define HOOK_DOWN_1_PIN 2
+#define HOOK_UP_1_PIN 3
+#define HOOK_UP_2_PIN 5
+#define DIAL_0_PIN 4
+#define SWITCH_PIN A1
+#define LED_PIN A0 //off-hook indicator LED
 
 #define BTN_POLL_INTERVAL 10 //ms
 #define DEBOUNCE_TIME 100 //ms
@@ -57,9 +57,9 @@ void SetLineOpen(uint8_t val)
 	//open line
 	if (val)
 	{
-		digitalWrite(HOOK0_PIN, LOW);
-		digitalWrite(HOOK1_PIN, LOW);
-		digitalWrite(HOOK2_PIN, LOW);
+		digitalWrite(HOOK_DOWN_1_PIN, LOW);
+		digitalWrite(HOOK_UP_1_PIN, HIGH);
+		digitalWrite(HOOK_UP_2_PIN, HIGH);
 		digitalWrite(LED_PIN, HIGH);
 		Serial.println("line open");
 		lineOpen = 1;
@@ -67,13 +67,21 @@ void SetLineOpen(uint8_t val)
 	//hang up
 	else
 	{
-		digitalWrite(HOOK0_PIN, HIGH);
-		digitalWrite(HOOK1_PIN, HIGH);
-		digitalWrite(HOOK2_PIN, HIGH);
+		digitalWrite(HOOK_DOWN_1_PIN, HIGH);
+		digitalWrite(HOOK_UP_1_PIN, LOW);
+		digitalWrite(HOOK_UP_2_PIN, LOW);
 		digitalWrite(LED_PIN, LOW);
 		Serial.println("line closed");
 		lineOpen = 0;
 	}
+}
+
+//
+void dial_0()
+{
+	digitalWrite(DIAL_0_PIN, HIGH);
+	delayWhileSwitchNotPressed(500);
+	digitalWrite(DIAL_0_PIN, LOW);				
 }
 
 //
@@ -87,17 +95,13 @@ void setup()
 
 	Timer1.initialize(BTN_POLL_INTERVAL * 1000);
 	Timer1.attachInterrupt(updateBtnState); 
-		
-	digitalWrite(LED_PIN, LOW);
-	digitalWrite(HOOK0_PIN, HIGH);
-	digitalWrite(HOOK1_PIN, HIGH);
-	digitalWrite(HOOK2_PIN, HIGH);
-	digitalWrite(DIAL_0_PIN, HIGH);
 	
+	SetLineOpen(0);
+ 	
 	pinMode(LED_PIN, OUTPUT);
-	pinMode(HOOK0_PIN, OUTPUT);
-	pinMode(HOOK1_PIN, OUTPUT);
-	pinMode(HOOK2_PIN, OUTPUT);
+	pinMode(HOOK_DOWN_1_PIN, OUTPUT);
+	pinMode(HOOK_UP_1_PIN, OUTPUT);
+	pinMode(HOOK_UP_2_PIN, OUTPUT);
 	pinMode(DIAL_0_PIN, OUTPUT);
 	pinMode(SWITCH_PIN, INPUT_PULLUP);
 	
@@ -128,16 +132,12 @@ void setup()
 					
 					//get to a live operator by dialing 0 through each menu			
 					Serial.println("dial tone, dialing 0");
-					digitalWrite(DIAL_0_PIN, LOW);
-					delayWhileSwitchNotPressed(500);
-					digitalWrite(DIAL_0_PIN, HIGH);				
+					dial_0();
 
 					delayWhileSwitchNotPressed(18000);
 
 					Serial.println("menu, dialing 0");
-					digitalWrite(DIAL_0_PIN, LOW);
-					delayWhileSwitchNotPressed(500);
-					digitalWrite(DIAL_0_PIN, HIGH);
+					dial_0();
 					
 					if (debouncedButtonState)
 						SetLineOpen(0);
